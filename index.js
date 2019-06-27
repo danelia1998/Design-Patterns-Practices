36a5938af15ce3bc46c5d5d1a4f3510b35383941
test <davit.danelia.1@btu.edu.ge>
2019-06-27 16:17:42 +0400

    fix(Fixing some bugs):

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ require('./build/factory/main.js');
 require('./build/abstract-factory/main.js');
 require('./build/prototype/main.js');
 require('./build/singleton/main.js');
+require('./build/builder/main.js');
